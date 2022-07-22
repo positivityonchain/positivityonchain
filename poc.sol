6a72e710ef75f226715defbd71210c4843e14f4e
positivityonchain <109624923+positivityonchain@users.noreply.github.com>
2022-07-22 10:52:00 +0100

    Update poc.sol

--- poc.sol
+++ poc.sol
@@ -13,7 +13,7 @@ pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import {Base64} from "./libraries/Base64.sol";
+import {Base64} from "./contracts/libraries/Base64.sol";
  
 contract positivityOnChain is ERC721URIStorage, Ownable {
   uint256 numClaimed = 0;
